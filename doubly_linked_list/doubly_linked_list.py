2cb0ab03df70e5efa79d8dae993311ba1cf4b147
RobertMisch <robertmisch94@gmail.com>
2020-06-09 18:21:33 -0400

    alittle over half of double linked finished

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -48,19 +48,54 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        if(self.length <= 0):
+            self.head=new_node
+            self.tail=new_node
+            self.length+=1
+        else:
+            # self.head.insert_before(new_node)
+            self.head.prev=new_node
+            new_node.next=self.head
+            self.head=new_node
+            self.length+=1
+            return self.head
+            
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node."""
     def remove_from_head(self):
-        pass
+        if(not self.head):
+            return None
+        else:
+            data=self.head.value
+            if(self.head.next):
+                self.head=self.head.next
+                self.head.prev=None
+            else:
+                self.head=None
+                self.tail=None
+                self.length-=1
+                return data
+            self.length-=1
+            return data
 
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        if(self.length <= 0):
+            self.head=new_node
+            self.tail=new_node
+            self.length+=1
+        else:
+            self.tail.next=new_node
+            new_node.prev=self.tail
+            self.tail=new_node
+            self.length+=1
+            return self.tail
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
